917413796244703b54da2b976bcfe72e00b59595
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-11-07 17:46:22 -0300

    style: add styles to MovieInfo component

---
+++ src/components/MovieInfo/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const MovieInfoStyled = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  & span {
+    display: flex;
+    gap: 2px;
+    padding: 4px;
+  }
+`;
