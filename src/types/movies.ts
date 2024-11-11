670cc144ff7f9c635ae5665d831c3aee40cce5b5
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-11-11 10:49:52 -0300

    feat: add Movie type received from api

---
+++ src/types/movies.ts
@@ -0,0 +1,56 @@
+type Movies = {
+  id: number;
+  url: string;
+  name: string;
+  type: string;
+  language: string;
+  genres: string[];
+  status: string;
+  runtime: number;
+  averageRuntime: number;
+  premiered: string;
+  ended: string;
+  officialSite: string;
+  schedule: {
+    time: string;
+    days: string[];
+  };
+  rating: {
+    average: number | null;
+  };
+  weight: number;
+  network: {
+    id: number;
+    name: string;
+    country: {
+      name: string;
+      code: string;
+      timezone: string;
+    };
+    officialSite: string;
+  } | null;
+  webChannel: null | string;
+  dvdCountry: null | string;
+  externals: {
+    tvrage: number;
+    thetvdb: number;
+    imdb: string;
+  };
+  image: {
+    medium: string;
+    original: string;
+  };
+  summary: string;
+  updated: number;
+  _links: {
+    self: {
+      href: string;
+    };
+    previousepisode?: {
+      href: string;
+      name?: string;
+    };
+  };
+}
+
+export type MoviesArray = Movies[];
