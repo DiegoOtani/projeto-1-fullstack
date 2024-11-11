e33cc03b7c8c0163db5eb5f40289dc41e32e6213
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-11-11 18:10:01 -0300

    feat: use createPortal to open DetailsModal and select showId

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import './App.css'
+import MovieSection from './Sections/MoviesSection'
+import DetailsModal from './components/DetailsModal'
+import ReactDOM from 'react-dom';
+import { useContext } from 'react';
+import { ModalContext } from './contexts/ModalContext';
 
 function App() {
+  const context = useContext(ModalContext);
 
-  return (
-    <>
-    </>
-  )
+  if (!context) throw new Error('Context problem');
+
+  const { detailModalOpen, setDetailModalOpen } = context;
+
+  const handleCloseModal = () => {
+    setDetailModalOpen(false);
+  }
+
+  return <main>
+    <MovieSection />
+    {detailModalOpen && (
+    ReactDOM.createPortal(
+      <DetailsModal onClick={handleCloseModal}/>,
+      document.getElementById('modal-root') as HTMLElement
+    )
+  )}
+  </main>
 }
 
 export default App
