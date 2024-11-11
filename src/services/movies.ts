5f9d42e3017a80e057f41b1a72489c9a444a893f
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-11-11 18:36:29 -0300

    feat: add getMovieById service

--- src/services/movies.ts
+++ src/services/movies.ts
@@ -1,11 +1,25 @@
 import { api } from "./api";
 import axios from "axios";
-import { MoviesArray } from "../types/movies";
+import { MoviesArray, Movies } from "../types/movies";
 
 class MoviesService {
   static async getMovies(): Promise<MoviesArray> {
     try {
       const response = await api.get('/shows'); 
+      return response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error('Error fetching types:', error.message);
+      } else {
+        console.error('Unexpected error:', error);
+      }
+      throw error;
+    }
+  };
+
+  static async getMovieById(id: number): Promise<Movies> {
+    try {
+      const response = await api.get(`shows/${id}`);
       return response.data;
     } catch (error) {
       if (axios.isAxiosError(error)) {
