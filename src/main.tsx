02b9d80b9a0510d68640e641e0c50c35adb5c606
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-11-11 21:53:35 -0300

    feat: add SearchContext

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ModalProvider } from './contexts/ModalContext.tsx'
+import { SearchProvider } from './contexts/SearchContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ModalProvider>
-      <App />
-    </ModalProvider>
+    <SearchProvider>
+      <ModalProvider>
+        <App />
+      </ModalProvider>
+    </SearchProvider>
   </StrictMode>,
 )
