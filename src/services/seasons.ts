abd0e6a9d08f697b6c473865d5c2aa88192f7662
Diego Otani <diegokiyoshiitabera@gmail.com>
2024-11-12 15:01:12 -0300

    feat: add getEpisodesBySeasonId service

--- src/services/seasons.ts
+++ src/services/seasons.ts
@@ -1,6 +1,7 @@
 import { api } from "./api";
 import axios from "axios";
 import { SeasonsArray } from "../types/seasons";
+import { EpisodesArray } from "../types/episodes";
 
 class SeasonService {
   static async getSeasons(showId: number): Promise<SeasonsArray> {
@@ -14,8 +15,22 @@ class SeasonService {
         console.error('Unexpected error:', error);
       }
       throw error;
-    }
-  }
+    };
+  };
+
+  static async getEpisodesBySeasonId(seasonId: number): Promise<EpisodesArray> {
+    try {
+      const response = await api.get(`/seasons/${seasonId}/episodes`);
+      return response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error('Error fetching types:', error.message);
+      } else {
+        console.error('Unexpected error:', error);
+      }
+      throw error;
+    };
+  };
 };
 
 export default SeasonService;
